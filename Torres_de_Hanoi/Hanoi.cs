051aaf4468c9781e1cc2c59ff586b54bdaf25cd4
daus_dog <mvmsueca@gmail.com>
2026-02-15 19:59:54 +0100

    clase hanoi actualizada

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -12,11 +12,54 @@ namespace Torres_de_Hanoi
         public void mover_disco(Pila a, Pila b)
         {
 
+            if (a.isEmpty == false ) {
+                
+                if (b.isEmpty == false) {
+
+                    if (a.Elementos[a.Top].Tamano < b.Elementos[b.Top].Tamano)
+                    {
+                        b.push(a.pop);
+                    }
+                }
+                else { b.push(a.pop); }                   
+            }
+            
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+            int m = 0;
+            if(n%2 = 0)
+            {
+                for (int i = 0; i < Math.Pow(2,n)-1; i++)
+                {
+                    mover_disco(ini, fin);
+                    m++;
+                    i++; //cada movimiento cuenta
+                    mover_disco(ini, aux);
+                    m++;
+                    i++;
+                    mover_disco(aux, fin);
+                    m++;
+                }
+               
+
+            }
+            else if (n % 2 != 0)
+            {
+                for (int i = 0; i < 2 ^ n - 1; i++)
+                {
+                    mover_disco(ini, aux);
+                    m++;
+                    i++; //cada movimiento cuenta
+                    mover_disco(ini, fin);
+                    m++;
+                    i++;
+                    mover_disco(aux, fin);
+                    m++;
+                }
+            }
+            return m;
         }
 
     }
