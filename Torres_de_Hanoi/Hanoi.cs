1d2171bee3c489ca32767a924bc330897cb0c50e
daus_dog <mvmsueca@gmail.com>
2026-02-21 17:36:50 +0100

    comentarios de hanoi

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -46,16 +46,19 @@ namespace Torres_de_Hanoi
 
         public int iterativo(int n, Pila ini, Pila aux ,Pila fin)
         {
-            int m = 0;
-            int totalMov = (1 << n) - 1;
+            int m = 0; // guarda todos los movimientos
 
+            // int totalMov = (1 << n) - 1;  ahorra la conversión de double utilizando
+            // números binarios, simplemente desplaza el “1” n veces a la izquierda y le restamos 1.
+            int totalMov = (1 << n) - 1; 
             Console.WriteLine("Situcion inicial ");
-            if (n % 2 == 0)
+            if (n % 2 == 0) // comprueba si es par o inpar
             {
                 
                 while (m < totalMov)
                 {
-                    if (mover_disco(ini, aux)) { m++; ImprimirEstado(m, ini, aux, fin); }
+                    //llama a mover discos en el orden de par
+                    if (mover_disco(ini, aux)) { m++; ImprimirEstado(m, ini, aux, fin); } 
                     if (m < totalMov && mover_disco(ini, fin)) { m++; ImprimirEstado(m, ini, aux, fin); }
                     if (m < totalMov && mover_disco(aux, fin)) { m++; ImprimirEstado(m, ini, aux, fin); }
                 }
@@ -77,42 +80,42 @@ namespace Torres_de_Hanoi
 
         public int Recursivo(int n, Pila ini, Pila aux, Pila fin)
         {
-            int movimientos = 0;
+            int m = 0; // guarda el numero de movimientos para devolverlos al final de la s comparativas
 
             void Hanoi(int discos, Pila origen, Pila auxiliar, Pila destino)
             {
-                if (discos == 1)
+                if (discos == 1) //comprueba si hay solo hay un disco en n
                 {
                     mover_disco(origen, destino);
-                    movimientos++;
-                    ImprimirEstado(movimientos, ini, aux, fin);
+                    m++;//aumenta los movimientos
+                    ImprimirEstado(m, ini, aux, fin); // llama a la funcion para imprimir por pantalla
                 }
-                else
+                else 
                 {
-                    Hanoi(discos - 1, origen, destino, auxiliar);
+                    Hanoi(discos - 1, origen, destino, auxiliar); //se vulve a llamar a si misma restando un uno para diferencia entre par y inpar
 
-                    mover_disco(origen, destino);
-                    movimientos++;
-                    ImprimirEstado(movimientos, ini, aux, fin); 
+                    mover_disco(origen, destino); //llama a la funcion para mover los discos
+                    m++;
+                    ImprimirEstado(m, ini, aux, fin); 
 
-                    Hanoi(discos - 1, auxiliar, origen, destino);
+                    Hanoi(discos - 1, auxiliar, origen, destino); // se encarga de cambiar el orden para el tercer paso
                 }
             }
-
+            //texto para la primera peticion
             Console.WriteLine("Situación inicial");
             ini.MostrarPila();
             aux.MostrarPila();
             fin.MostrarPila();
             Console.WriteLine("---------------------------");
 
-            Hanoi(n, ini, aux, fin);
+            Hanoi(n, ini, aux, fin); //llama a la funcion publica hanoi 
 
-            return movimientos;
+            return m;
         }
-
+        //funcion para imprimir resultados
         void ImprimirEstado(int mov, Pila ini, Pila aux, Pila fin)
         {
-            Console.WriteLine($"\nsituación del movimiento {mov}");
+            Console.WriteLine($"\nsituación del movimiento {mov}");//\n: permite un salto de pagina   $: permite poner variables ne el texto
             ini.MostrarPila();
             aux.MostrarPila();
             fin.MostrarPila();
